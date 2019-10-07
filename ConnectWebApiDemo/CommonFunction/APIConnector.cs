bc774377ae2d2a57ff6bdd35250f419169132be5
Leo <leoyang@jepun.com.tw>
2019-10-07 11:13:09 +0800

    Connect to WebApi demo first version commit

---
+++ ConnectWebApiDemo/CommonFunction/APIConnector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectWebApiDemo.CommonFunction
+{
+    public static class APIConnector
+    {
+        public static string Get(string url, Dictionary<string, string> headers = null, Func<HttpWebRequest, HttpWebResponse, string> hook = null)
+        {
+            string result = "ErrorCode:404";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.Accept = "application/json";
+                request.ContentType = "application/json";
+                if (headers != null)
+                {
+                    foreach (string key in headers.Keys)
+                    {
+                        request.Headers[key] = headers[key];
+                    }
+                }
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        if (hook == null)
+                        {
+                            using (Stream sm = response.GetResponseStream())
+                            using (StreamReader reader = new StreamReader(sm))
+                            {
+                                result = reader.ReadToEnd();
+                            }
+                        }
+                        else
+                            result = hook(request, response);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ ConnectWebApiDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectWebApiDemo.CommonFunction;
+
+namespace ConnectWebApiDemo
+{
+    class Program
+    {
+        #region Memberfunction
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:63207/Api/Test/Get";
+            JObject jo = JObject.Parse(APIConnector.Get(string.Format(url, 1000, 0)));
+            Console.WriteLine(jo.GetValue("status"));
+            Console.WriteLine(jo.GetValue("message"));
+            foreach (JObject item in jo.Property("data").Value as JArray)
+            {
+                Console.WriteLine(item["stockId"]);
+                Console.WriteLine(item["stockName"]);
+                Console.WriteLine(item["exchange"]);
+                Console.WriteLine(item["issued"]);
+            }
+        }
+        #endregion
+    }
+}
